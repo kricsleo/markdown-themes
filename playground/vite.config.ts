20828f77b59b8f3975d2372913af9b976f1991b3
许圣峰 <kricsleo@163.com>
2023-05-13 23:42:08 +0800

    chore: playground

---
+++ playground/vite.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest" />
+
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+    Vue(),
+    // https://github.com/antfu/unocss => unocss.config.ts
+    Unocss(),
+  ],
+})
